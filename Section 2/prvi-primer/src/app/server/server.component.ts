23e77bcd8ffb8d31454af15436455ddf2443bccf
dejan <dekac9@yahoo.com>
2021-11-10 13:16:01 +0100

    string data binding

--- Section 2/prvi-primer/src/app/server/server.component.ts
+++ Section 2/prvi-primer/src/app/server/server.component.ts
@@ -6,5 +6,11 @@ import { Component } from "@angular/core";
 })
 
 export class ServerComponent{
+  probaZaMetodu="OPALAC"
+serverId: number=10;
+serverStatus:string='offline'
+getServerStatus(){
+  return this.probaZaMetodu
+}
 
 }
