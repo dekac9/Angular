a68ca490003660278604fed2e1e22c61a447170d
dejan <dekac9@yahoo.com>
2021-11-15 16:35:53 +0100

    Zavrseno vezbanje

--- Section 2/3 vezba direktive/vezba/src/app/komponenta/komponenta.component.ts
+++ Section 2/3 vezba direktive/vezba/src/app/komponenta/komponenta.component.ts
@@ -3,13 +3,36 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-komponenta',
   templateUrl: './komponenta.component.html',
-  styleUrls: ['./komponenta.component.css']
+  styleUrls: ['./komponenta.component.css'],
+  styles:[
+    `.hidden{
+      visibility:hidden
+    }
+    .belo{
+      color:white
+    }
+    .plavo{
+      background:blue;
+      color:white
+    }`
+
+  ]
 })
 export class KomponentaComponent implements OnInit {
-
+toggle=false
+start=0
+array=[0]
+boja='belo'
   constructor() { }
 
   ngOnInit(): void {
   }
+toggluj(){
+  this.toggle===false?this.toggle=true:this.toggle=false
+  this.start++
+  this.array.push(this.start)
+  console.log(this.start)
+  console.log(this.array)
+}
 
 }
