5cff96b52e075025c3e7a4ef5aeb6aa5f6b6ad7a
dejan <dekac9@yahoo.com>
2021-11-16 12:56:41 +0100

    dodat kodel (konstruktor) svakog recepta

--- Section 3/projekat/src/app/recepies/recepie-list/recepie-list.component.ts
+++ Section 3/projekat/src/app/recepies/recepie-list/recepie-list.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recepie-list.component.css']
 })
 export class RecepieListComponent implements OnInit {
-
+recepies=[];
   constructor() { }
 
   ngOnInit(): void {
