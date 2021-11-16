f3af72d32abd6a063984047468e0025d99e8c807
dejan <dekac9@yahoo.com>
2021-11-16 18:23:23 +0100

    nastavak rada na komponentama

--- Section 3/projekat/src/app/recepies/recepie-list/recepie-list.component.ts
+++ Section 3/projekat/src/app/recepies/recepie-list/recepie-list.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recepie } from '../recepie.model';
 @Component({
   selector: 'app-recepie-list',
   templateUrl: './recepie-list.component.html',
   styleUrls: ['./recepie-list.component.css']
 })
 export class RecepieListComponent implements OnInit {
-recepies=[];
+recepies:Recepie[]=[
+  new Recepie('Test recept', 'Ovo je samo test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
+  new Recepie("Drugi recept", 'Drugi opis', 'https://www.simplyrecipes.com/thmb/UyCJN4g__IiBR8aHOJ9yKd8kcSk=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2015__01__cheesy-bread-vertical-a-1800-6b76aa9ce7024535af1901d8a9a2e458.jpg')
+];
   constructor() { }
 
   ngOnInit(): void {
