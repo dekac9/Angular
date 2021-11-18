800bfaa7006deb5a705d9d15c2077f1d2d157942
dejan <dekac9@yahoo.com>
2021-11-18 17:02:21 +0100

    Zavrseno emiteri child to parent

--- Section 5/Zadatak 1/src/app/app.component.ts
+++ Section 5/Zadatak 1/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [{type:"server",name:"Testserver",content:"Samo test!"}];
+  serverElements = [{type:"blueprint",name:"Testserver",content:"Samo test!"}];
 
   onServerAdded(serverData:{serverName:string,serverContent:string}) {
     this.serverElements.push({
@@ -16,7 +16,7 @@ export class AppComponent {
     });
   }
 
-  onBlueprintAdded(blueprintData:{serverName:string, serverContent:string}) {
+  onblueprintAdded(blueprintData:{serverName:string, serverContent:string}) {
     this.serverElements.push({
       type: 'blueprint',
       name: blueprintData.serverName,
