827fd3c0bca27b75e6864c585bb7202b55237187
dejan <dekac9@yahoo.com>
2021-11-22 11:08:31 +0100

    pocetak zadatka

---
+++ Section 5/Zadatak na kraju sekcije/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
+import { AppComponent } from './app.component';
+import { GameControllComponent } from './game-controll/game-controll.component';
+import { OddComponent } from './odd/odd.component';
+import { EvenComponent } from './even/even.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GameControllComponent,
+    OddComponent,
+    EvenComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Section 5/Zadatak na kraju sekcije/src/app/game-controll/game-controll.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-controll',
+  templateUrl: './game-controll.component.html',
+  styleUrls: ['./game-controll.component.css']
+})
+export class GameControllComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
