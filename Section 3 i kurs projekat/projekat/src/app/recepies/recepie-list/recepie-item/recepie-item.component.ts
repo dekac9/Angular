6e1d6ff98822c3bbbeba7a2b04bfbad5e99ce25b
dejan <dekac9@yahoo.com>
2021-11-23 13:06:35 +0100

    nastavak rada data passing

--- Section 3 i kurs projekat/projekat/src/app/recepies/recepie-list/recepie-item/recepie-item.component.ts
+++ Section 3 i kurs projekat/projekat/src/app/recepies/recepie-list/recepie-item/recepie-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-recepie-item',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recepie-item.component.css']
 })
 export class RecepieItemComponent implements OnInit {
-
+@Input() recepie
   constructor() { }
 
   ngOnInit(): void {
