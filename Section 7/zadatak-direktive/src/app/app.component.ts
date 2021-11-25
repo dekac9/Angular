3738a611af4ecd23457b49fd0a08a18fc59cc121
dejan <dekac9@yahoo.com>
2021-11-25 13:14:36 +0100

    Proezbavanje direktiva

--- Section 7/zadatak-direktive/src/app/app.component.ts
+++ Section 7/zadatak-direktive/src/app/app.component.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  numbers = [1, 2, 3, 4, 5];
-  onlyOdd = false;
+  //numbers = [1, 2, 3, 4, 5];
+  neparniBrojevi=[1,3,5];
+  parniBrojevi=[2,4]
+  onlyParni = false;
 }
