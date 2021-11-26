77f7fc7ce062a2d4dcc883a52d4bbbcb2ffdf1e8
dejan <dekac9@yahoo.com>
2021-11-26 10:04:05 +0100

    host binding decorator

--- Section 7/zadatak-direktive/src/app/bolje-osvetli/bolje-osvetli.directive.ts
+++ Section 7/zadatak-direktive/src/app/bolje-osvetli/bolje-osvetli.directive.ts
@@ -1,10 +1,19 @@
 
-import { Directive, Renderer2 ,OnInit, ElementRef, HostListener} from '@angular/core';
+import { 
+  Directive, 
+  Renderer2 ,
+  OnInit, 
+  ElementRef, 
+  HostListener,
+  HostBinding
+} from '@angular/core';
 
 @Directive({
   selector: '[appBoljeOsvetli]'
 })
 export class BoljeOsvetliDirective implements OnInit{
+
+  @HostBinding('style.backgroundColor')backgroundColor:string='transparent'
 
   constructor(private elRef:ElementRef,private renderer:Renderer2) { }
   ngOnInit() {
@@ -12,10 +21,12 @@ export class BoljeOsvetliDirective implements OnInit{
     
   }
   @HostListener('mouseenter') mouseover(eventData: Event){
-this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'pink', )
+//this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'pink', )
+this.backgroundColor='purple'
 }
 @HostListener('mouseleave') mouseleave(eventData: Event){
-this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'orange', )
+//this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'orange', )
+this.backgroundColor='transparent'
 }
 }
 
