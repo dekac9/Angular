bf1cbd1a78bb0390a4f51aca4836e643dff8ec07
dejan <dekac9@yahoo.com>
2021-11-26 10:51:24 +0100

    predjene strukturne direktive

--- Section 7/zadatak-direktive/src/app/bolje-osvetli/bolje-osvetli.directive.ts
+++ Section 7/zadatak-direktive/src/app/bolje-osvetli/bolje-osvetli.directive.ts
@@ -5,24 +5,29 @@ import {
   OnInit, 
   ElementRef, 
   HostListener,
-  HostBinding
+  HostBinding,
+  Input
 } from '@angular/core';
 
 @Directive({
   selector: '[appBoljeOsvetli]'
 })
 export class BoljeOsvetliDirective implements OnInit{
-
-  @HostBinding('style.backgroundColor')backgroundColor:string='transparent'
+brojac:number=0
+@Input()defaultColor='transparent'
+@Input()novaBoja='brown'
+  @HostBinding('style.backgroundColor')backgroundColor:string
 
   constructor(private elRef:ElementRef,private renderer:Renderer2) { }
   ngOnInit() {
    //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'red', )
-    
+    this.backgroundColor=this.defaultColor
   }
   @HostListener('mouseenter') mouseover(eventData: Event){
 //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'pink', )
 this.backgroundColor='purple'
+this.brojac++
+console.log(this.brojac);
 }
 @HostListener('mouseleave') mouseleave(eventData: Event){
 //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'orange', )
