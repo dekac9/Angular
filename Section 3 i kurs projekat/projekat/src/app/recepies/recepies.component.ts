f5a7fe9cb73f7fcf2e60a373172ca056240fe832
dejan <dekac9@yahoo.com>
2021-12-02 13:52:36 +0100

    Ubacivanje servisa on init life hook

--- Section 3 i kurs projekat/projekat/src/app/recepies/recepies.component.ts
+++ Section 3 i kurs projekat/projekat/src/app/recepies/recepies.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { RecepieService } from './recepie.service';
 
 @Component({
   selector: 'app-recepies',
   templateUrl: './recepies.component.html',
-  styleUrls: ['./recepies.component.css']
+  styleUrls: ['./recepies.component.css'],
+  providers:[RecepieService]
 })
 export class RecepiesComponent implements OnInit {
 
