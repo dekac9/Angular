e339bb4d3a25dc500fd7e20740ed46cbe5d0ca3c
dejan <dekac9@yahoo.com>
2021-12-11 12:16:49 +0100

    nastavak

--- Section 11/Rutiranje-start/src/app/app.module.ts
+++ Section 11/Rutiranje-start/src/app/app.module.ts
@@ -11,6 +11,13 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import {RouterModule, Routes } from '@angular/router';
+
+const appRoute:Routes=[
+  {path:'home', component:HomeComponent},
+  {path:'servers', component:ServerComponent},
+  {path:'users', component:UsersComponent}
+]
 
 @NgModule({
   declarations: [
@@ -25,6 +32,7 @@ import { ServersService } from './servers/servers.service';
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(appRoute)
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
--- Section 11/Rutiranje-start/src/app/servers/servers.component.ts
+++ Section 11/Rutiranje-start/src/app/servers/servers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ServersService } from './servers.service';
 
 @Component({
@@ -9,10 +10,12 @@ import { ServersService } from './servers.service';
 export class ServersComponent implements OnInit {
   public servers: {id: number, name: string, status: string}[] = [];
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService, private router:Router) { }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
   }
-
+onReload(){
+this.router.navigate(['/servers'])
+}
 }
