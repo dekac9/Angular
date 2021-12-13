80440f8020c86455073da8d4285b87b1107555ac
dejan <dekac9@yahoo.com>
2021-12-13 13:46:15 +0100

    outsorsing ruta u poseban modul

---
+++ Section 11/Rutiranje-start/src/app/app-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from "@angular/core";
+import {RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { EditServerComponent } from "./servers/edit-server/edit-server.component";
+import { ServerComponent } from "./servers/server/server.component";
+import { ServersComponent } from "./servers/servers.component";
+import { UserComponent } from "./users/user/user.component";
+import { UsersComponent } from "./users/users.component";
+
+
+const appRoute:Routes=[
+  {path:'home', component:HomeComponent},  
+  {path:'users', component:UsersComponent, children:[
+    {path:':id/:name', component:UserComponent},
+  ]},
+  
+  {path:'servers', component:ServersComponent, children:[
+    {path:':id/edit', component:EditServerComponent},
+    {path:':id', component:ServerComponent}
+  ]},
+  {path:'not-found', component:PageNotFoundComponent},
+  {path:'**', redirectTo:'/not-found'}
+  
+]
+@NgModule({
+imports: [    
+    RouterModule.forRoot(appRoute)
+  ],
+  exports:[
+    RouterModule
+  ]
+})
+export class AppRoutingModule{
+
+}
--- Section 11/Rutiranje-start/src/app/app.module.ts
+++ Section 11/Rutiranje-start/src/app/app.module.ts
@@ -11,21 +11,11 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-import {RouterModule, Routes } from '@angular/router';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const appRoute:Routes=[
-  {path:'home', component:HomeComponent},  
-  {path:'users', component:UsersComponent, children:[
-    {path:':id/:name', component:UserComponent},
-  ]},
-  
-  {path:'servers', component:ServersComponent, children:[
-    {path:':id/edit', component:EditServerComponent},
-    {path:':id', component:ServerComponent}
-  ]},
-  
-]
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
+
+
 
 @NgModule({
   declarations: [
@@ -41,7 +31,7 @@ const appRoute:Routes=[
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoute)
+    AppRoutingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
