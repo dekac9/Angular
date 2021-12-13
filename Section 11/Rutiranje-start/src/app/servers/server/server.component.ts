af042d42a9f451100c63dda32942c1322feabf38
dejan <dekac9@yahoo.com>
2021-12-13 20:05:45 +0100

    zavrseno rutiranje

--- Section 11/Rutiranje-start/src/app/servers/server/server.component.ts
+++ Section 11/Rutiranje-start/src/app/servers/server/server.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
-import { relative } from 'path';
+
 
 import { ServersService } from '../servers.service';
 
