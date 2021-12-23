60975fc3fece90ea86aa329c9a35815900f39b81
dejan <dekac9@yahoo.com>
2021-12-23 11:40:24 +0100

    nastavak vezbanja

--- Section 15/forms-td-assignment-start/src/app/app.component.ts
+++ Section 15/forms-td-assignment-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+upisanMail=false
+upisanPass=false
+placeholderSelekta="advanced"
+sve={
+  mail:"",
+  vrsta:"",
+  pass:""
+}
+ispravno=false
+
+onSubmit(celaforma:NgForm){
+this.upisanMail=true ?(!celaforma.form.controls.eposta.valid && celaforma.form.controls.eposta.untouched):false
+  console.log(this.upisanMail);
+  if(celaforma.form.valid && !celaforma.form.untouched){
+    this.ispravno=true
+  this.sve.mail=celaforma.form.value.eposta
+  this.sve.vrsta=celaforma.form.value.vrstaPretplate
+  this.sve.pass=celaforma.form.value.tajnaLozinka
+  console.log("ispravno");
+console.log(celaforma.form.value);
+  }else{
+    console.log("nije ispravna forma")
+  }
+  
+
+}
+
 
 }
