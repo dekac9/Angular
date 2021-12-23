0de15cf0a37ac030d7f7eae616ff7445ec3c58a9
dejan <dekac9@yahoo.com>
2021-12-23 16:01:12 +0100

    zavrsena vezba TD forme

--- Section 15/forms-td-assignment-start/src/app/app.component.ts
+++ Section 15/forms-td-assignment-start/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -17,12 +17,19 @@ sve={
 }
 ispravno=false
 
-onSubmit(celaforma:NgForm){
-this.upisanMail=true ?(!celaforma.form.controls.eposta.valid && celaforma.form.controls.eposta.untouched):false
-  console.log(this.upisanMail);
+onSubmit(celaforma:NgForm ){
+// this.upisanMail=true ?(!celaforma.form.controls.eposta.valid && celaforma.form.controls.eposta.untouched):false
+if(!celaforma.form.controls.email.valid && !celaforma.form.controls.email.untouched ){
+this.upisanMail=true
+}
+if(!celaforma.form.controls.tajnaLozinka.valid && !celaforma.form.controls.email.untouched ){
+this.upisanPass=true
+}
+ console.log(celaforma.form.controls.email.valid);
   if(celaforma.form.valid && !celaforma.form.untouched){
-    this.ispravno=true
-  this.sve.mail=celaforma.form.value.eposta
+this.ispravno=true
+ 
+  this.sve.mail=celaforma.form.value.email
   this.sve.vrsta=celaforma.form.value.vrstaPretplate
   this.sve.pass=celaforma.form.value.tajnaLozinka
   console.log("ispravno");
