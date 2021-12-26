a23cafbfc4d12214442f31b687e879f3c20492d0
dejan <dekac9@yahoo.com>
2021-12-26 15:46:59 +0100

    gotovo za danas, sutra nastavak REACTiVE formi

--- Section 15/forms-reactive-start/src/app/app.component.ts
+++ Section 15/forms-reactive-start/src/app/app.component.ts
@@ -12,8 +12,10 @@ export class AppComponent implements OnInit{
 
   ngOnInit(): void {
      this.forma = new FormGroup({
-       'username': new FormControl(null, Validators.required),
-       'email': new FormControl(null, [Validators.required, Validators.email]),
+       'userData':new FormGroup({
+         'username': new FormControl(null, Validators.required),
+         'email': new FormControl(null, [Validators.required, Validators.email])
+       }),       
        'gender':new FormControl('male')
      })
   }
