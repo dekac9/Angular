187d0134200889ec9fc85a6ee95549b410e00ee1
dejan <dekac9@yahoo.com>
2021-12-26 15:11:44 +0100

    nastavak ReACTivE  pristupa input-u

--- Section 15/forms-reactive-start/src/app/app.component.ts
+++ Section 15/forms-reactive-start/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   genders = ['male', 'female'];
+  forma:FormGroup;
+
+  ngOnInit(): void {
+     this.forma = new FormGroup({
+       'username': new FormControl(null, Validators.required),
+       'email': new FormControl(null, [Validators.required, Validators.email]),
+       'gender':new FormControl('male')
+     })
+  }
+
+  onSubmit(){
+    console.log(this.forma);
+  }
 }
