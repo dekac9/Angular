1b228e9f0a8556ff2d8f4bc601202d6b26059dce
dejan <dekac9@yahoo.com>
2022-01-03 11:28:46 +0100

    nastavak

--- Section 15/forms-reactive-start/src/app/app.component.ts
+++ Section 15/forms-reactive-start/src/app/app.component.ts
@@ -9,15 +9,16 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 export class AppComponent implements OnInit{
   genders = ['male', 'female'];
   forma:FormGroup;
+  zabranjeniUsernames=['Ana', 'Misa']
 
   ngOnInit(): void {
      this.forma = new FormGroup({
        'userData':new FormGroup({
-         'username': new FormControl(null, Validators.required),
+         'username': new FormControl(null, [Validators.required,this.zabranjenaImena.bind(this)]),
          'email': new FormControl(null, [Validators.required, Validators.email])
        }),       
        'gender':new FormControl('male'),
-       'hobbies': new FormArray([])
+       'hobbiji': new FormArray([])
      })
   }
 
@@ -25,8 +26,25 @@ export class AppComponent implements OnInit{
     console.log(this.forma);
   }
 
-  onDodajHobi(){
-    const control=new FormControl(null, Validators.required);
-(<FormArray>this.forma.get('hobbies')).push(control)
+  zabranjenaImena(controll:FormControl):{[s:string]:boolean}{
+if(this.zabranjeniUsernames.indexOf(controll.value)!==-1){
+  return{'imeZabranjeno':true}
+}
+return null
   }
+
+//   onDodajHobi(){
+//     const control=new FormControl(null, Validators.required);
+// (<FormArray>this.forma.get('hobbiji')).push(control)
+// console.log(this.forma);
+
+//   }
+
+// onDodajHobi(){
+//   this.hobbiji.pus
+// }
+
+//  get hobbiji() {
+//   return this.forma.controls[this.hobbiji] as FormControl
+// }
 }
