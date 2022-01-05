1befdad3b11d624a3bf04382a660554def152c78
dejan <dekac9@yahoo.com>
2022-01-05 13:42:55 +0100

    custom pipe

--- Section 17/pipes-start/src/app/app.module.ts
+++ Section 17/pipes-start/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { Skracivanje } from './skracivanje.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Skracivanje
   ],
   imports: [
     BrowserModule,
--- Section 17/pipes-start/src/app/skracivanje.pipe.ts
+++ Section 17/pipes-start/src/app/skracivanje.pipe.ts
@@ -1,8 +1,13 @@
-import { PipeTransform } from "@angular/core";
+import { Pipe, PipeTransform } from "@angular/core";
 
-
+@Pipe({
+  name:'kratko'
+})
 export class Skracivanje implements PipeTransform{
-transform(value:any){
-
+transform(value:any, limit:number){
+  if(value.length>limit){
+    return value.slice(0,limit)+' ...'
+  }
+return value
 }
 }
