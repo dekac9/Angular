f1ce894461606a305dfaccbcfb6765dd97ac8b00
dejan <dekac9@yahoo.com>
2022-01-05 14:53:17 +0100

    zavrsetak filter pipea

--- Section 17/pipes-start/src/app/app.module.ts
+++ Section 17/pipes-start/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { Skracivanje } from './skracivanje.pipe';
+import { FilterPipe } from './filter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Skracivanje
+    Skracivanje,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
