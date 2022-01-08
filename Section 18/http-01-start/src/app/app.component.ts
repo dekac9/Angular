c1fabb0968c853bfa7041e1b67f562afe1171639
dejan <dekac9@yahoo.com>
2022-01-08 14:58:52 +0100

    rxjs operator map

--- Section 18/http-01-start/src/app/app.component.ts
+++ Section 18/http-01-start/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import{ map }from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
@@ -33,6 +35,18 @@ export class AppComponent implements OnInit {
   }
 
   private getPosts(){
-    this.http.get('https://ng-backend-projct-start-default-rtdb.firebaseio.com/posts.json').subscribe(posts=>{console.log(posts);})
+    this.http.get('https://ng-backend-projct-start-default-rtdb.firebaseio.com/posts.json').pipe(map(responseData=>{
+
+      const postsArray=[];
+      
+      for (const key in responseData) {
+if(responseData.hasOwnProperty(key)){
+postsArray.push({...responseData[key],id:key})
+}
+        
+
+      }
+      return postsArray
+    })).subscribe(posts=>{console.log(posts)})
   }
 }
