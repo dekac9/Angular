cb23e676d7d2d93c0f5d217d2d9927afb5dca7d1
dejan <dekac9@yahoo.com>
2022-01-16 15:10:59 +0100

    prvisagitom commit

--- Section 7/zadatak-direktive/src/app/app.component.ts
+++ Section 7/zadatak-direktive/src/app/app.component.ts
@@ -1,11 +1,23 @@
+import {MatSnackBar} from '@angular/material/snack-bar';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+  
+
 export class AppComponent {
+
+constructor(private _snackBar: MatSnackBar) {}
+
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action,{duration:1000, horizontalPosition:'right'});
+  }
+
   //numbers = [1, 2, 3, 4, 5];
   neparniBrojevi=[1,3,5];
   parniBrojevi=[2,4]
--- Section 7/zadatak-direktive/src/app/app.module.ts
+++ Section 7/zadatak-direktive/src/app/app.module.ts
@@ -7,6 +7,12 @@ import { AppComponent } from './app.component';
 import { BasicOsvetliDirektiva } from './osvetli/basic-osvetli.directive';
 import { BoljeOsvetliDirective } from './bolje-osvetli/bolje-osvetli.directive';
 import { SuprotnoDirective } from './suprotno.directive';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+
 
 @NgModule({
   declarations: [
@@ -18,6 +24,11 @@ import { SuprotnoDirective } from './suprotno.directive';
   imports: [
     BrowserModule,
     FormsModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatSnackBarModule,
+    MatFormFieldModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
